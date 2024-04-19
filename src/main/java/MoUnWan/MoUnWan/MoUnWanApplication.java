61377e8bbea5c81673b2a73efbb18e39b5cd2989
ohhyerin <lyneen@naver.com>
2024-04-19 13:36:06 +0900

    first commit

---
+++ src/main/java/MoUnWan/MoUnWan/MoUnWanApplication.java
@@ -0,0 +1,13 @@
+package MoUnWan.MoUnWan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoUnWanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoUnWanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MoUnWan/MoUnWan/MoUnWanApplicationTests.java
@@ -0,0 +1,13 @@
+package MoUnWan.MoUnWan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoUnWanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
