36456fc2ec1098bafa803eb13d3e1c854765912c
ohhyerin <lyneen@naver.com>
2024-05-30 21:34:42 +0900

    deploy.sh 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/mounwan
 cd $REPOSITORY
 
-APP_NAME=mounwan-project-server
+APP_NAME=mounwan_cicid
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
