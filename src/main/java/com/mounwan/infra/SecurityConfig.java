208ef3bd7782d254e097ab9eae1d0ae35c685eed
ohhyerin <lyneen@naver.com>
2024-06-15 18:58:56 +0900

    시큐리티설정 코드수정

--- src/main/java/com/mounwan/infra/SecurityConfig.java
+++ src/main/java/com/mounwan/infra/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.mounwan.infra;
 
 import com.mounwan.moudules.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
@@ -25,9 +27,10 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+        http.authorizeHttpRequests(requestMatcherRegistry->requestMatcherRegistry
                 .requestMatchers("/", "/login", "sign-up", "checked-email", "check-email-token",
                         "/email-login", "/login-link").permitAll()
+                .requestMatchers("/node_moudules/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/profile/*").permitAll() //get요청만 허용
                 .anyRequest().authenticated()); //나머지 요청은 로그인해야 가능
 
@@ -45,11 +48,14 @@ public class SecurityConfig{
         return  http.build();
     }
 
+
     @Bean
     public PersistentTokenRepository tokenRepository() {
         JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
         jdbcTokenRepository.setDataSource(dataSource);
         return jdbcTokenRepository;
     }
+
+
 
 }
