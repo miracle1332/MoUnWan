b4cb9246181ba92224db34b5f25bec16acfca926
ohhyerin <lyneen@naver.com>
2024-06-15 18:59:14 +0900

    회원가입검증객체 생성

--- src/main/java/com/mounwan/moudules/SignUpFormValidator.java
+++ src/main/java/com/mounwan/moudules/SignUpFormValidator.java
@@ -9,14 +9,26 @@ import org.springframework.validation.Validator;
 @RequiredArgsConstructor
 public class SignUpFormValidator implements Validator {
 
+    private final AccountRepository accountRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+            return clazz.isAssignableFrom(SignUpForm.class);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm)target;
+        if(accountRepository.existsByEmail(signUpForm.getEmail())) {
+            errors.rejectValue("email","invalid.email",
+                    new Object[]{signUpForm.getEmail()},"이미 사용중인 이메일입니다.");
+        }
+
+        if(accountRepository.existsByNickname(signUpForm.getNickname())) {
+            errors.rejectValue("nickname","invalid.nickname",
+                    new Object[]{signUpForm.getNickname()},"이미 사용중인 닉네임입니다.");
+        }
+
 
     }
 }
