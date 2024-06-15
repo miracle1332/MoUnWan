dbaa9dd4674f3bc94ac0b2acfaa015230ec4c471
ohhyerin <lyneen@naver.com>
2024-06-15 17:01:22 +0900

    tokenRepository() 생성

--- src/main/java/com/mounwan/infra/SecurityConfig.java
+++ src/main/java/com/mounwan/infra/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mounwan.infra;
 
 
+import com.mounwan.moudules.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +9,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.sql.DataSource;
 
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig{
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,9 +38,18 @@ public class SecurityConfig{
         http.logout((logout) -> logout
                 .logoutSuccessUrl("/"));
 
-
+        http.rememberMe(httpSecurityRememberMeConfigurer -> httpSecurityRememberMeConfigurer
+                .userDetailsService(accountService)
+                .tokenRepository(tokenRepository()));
 
         return  http.build();
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
 }
